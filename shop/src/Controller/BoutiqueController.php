a4185b9ff6f250abb6627acdf2fde059a87feab3
Yohan <yohan.g99@gmail.com>
2020-12-07 16:02:13 +0100

    init

---
+++ shop/src/Controller/BoutiqueController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\BoutiqueService;
+
+class BoutiqueController extends AbstractController {
+
+    /**
+    * @Route("/boutique")
+    */
+    public function index(BoutiqueService $boutique) {
+        //$categories = $boutique->findAllCategories();
+        return $this->render('base.html.twig', [
+            'number' => 25,
+        ]);
+    }
+}
