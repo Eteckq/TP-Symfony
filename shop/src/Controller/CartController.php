490831529eaae30323bca797b3629dfb2d0db550
Yohan <yohan.g99@gmail.com>
2020-12-08 21:37:59 +0100

    start cart

--- shop/src/Controller/CartController.php
+++ shop/src/Controller/CartController.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Controller;
+
+use App\Entity\Article;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,8 +12,36 @@ class CartController extends AbstractController {
     * @Route("/{_locale}/cart", name="cart", requirements={"_locale": "fr|en|es"})
     */
     public function index() {
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/cart.html.twig', [
             'menu' => "cart",
         ]);
+    }
+
+    /**
+    * @Route("/cart/add/{id}", name="cart.add")
+    */
+    public function addToCart(Article $article) {
+        return $this->redirectToRoute("cart");
+    }
+
+    /**
+    * @Route("/cart/remove/{id}", name="cart.remove")
+    */
+    public function removeFromCart(Article $article) {
+        return $this->redirectToRoute("cart");
+    }
+
+    /**
+    * @Route("/cart/delete/{id}", name="cart.delete")
+    */
+    public function deleteFromCart(Article $article) {
+        return $this->redirectToRoute("cart");
+    }
+
+    /**
+    * @Route("/cart/empty", name="cart.empty")
+    */
+    public function emptyCart() {
+        return $this->redirectToRoute("cart");
     }
 }
