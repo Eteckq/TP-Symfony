4856cf1d554fdf3b4e3784b6712c9942d3642079
Yohan <yohan.g99@gmail.com>
2020-12-09 16:17:41 +0100

    add security

--- shop/src/Controller/AdminController.php
+++ shop/src/Controller/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController {
 
     /**
-    * @Route("/{_locale/admin}", name="admin", requirements={"_locale": "fr|en|es"})
+    * @Route("/{_locale}/admin", name="admin", requirements={"_locale": "fr|en|es"})
     */
     public function index() {
         return $this->render('admin/index.html.twig', [
