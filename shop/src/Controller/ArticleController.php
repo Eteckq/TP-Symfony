74b34f5912a638e58fa6c3e0dbd6fd18243596e3
Yohan <yohan.g99@gmail.com>
2020-12-09 17:03:55 +0100

    refactoring

--- shop/src/Controller/ArticleController.php
+++ shop/src/Controller/ArticleController.php
@@ -11,10 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
- * @Route("/article")
- */
+* @Route("/{_locale}/article", requirements={"_locale"="fr|en|es"})
+* @IsGranted("ROLE_ADMIN")
+*/
 class ArticleController extends AbstractController
 {
     /**
